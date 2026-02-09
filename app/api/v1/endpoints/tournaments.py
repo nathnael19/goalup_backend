da982d2027f7875b521c2e459234a98373d0b3c7
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-02-09 19:22:19 +0300

    created a create functionlity for tournament

--- app/api/v1/endpoints/tournaments.py
+++ app/api/v1/endpoints/tournaments.py
@@ -2,11 +2,19 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 from app.core.database import get_session
-from app.models.tournament import Tournament
+from app.models.tournament import Tournament, TournamentCreate, TournamentRead
 
 router = APIRouter()
 
-@router.get("/", response_model=List[Tournament])
+@router.post("/", response_model=TournamentRead)
+def create_tournament(*, session: Session = Depends(get_session), tournament: TournamentCreate):
+    db_tournament = Tournament.model_validate(tournament)
+    session.add(db_tournament)
+    session.commit()
+    session.refresh(db_tournament)
+    return db_tournament
+
+@router.get("/", response_model=List[TournamentRead])
 def read_tournaments(session: Session = Depends(get_session)):
     tournaments = session.exec(select(Tournament)).all()
     return tournaments
--- app/models/tournament.py
+++ app/models/tournament.py
@@ -2,11 +2,21 @@ import uuid
 from typing import Optional, List
 from sqlmodel import Field, SQLModel, Relationship
 
-class Tournament(SQLModel, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+class TournamentBase(SQLModel):
     name: str
     year: int
     type: str
 
+class Tournament(TournamentBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+
     matches: List["Match"] = Relationship(back_populates="tournament")
     standings: List["Standing"] = Relationship(back_populates="tournament")
+
+class TournamentCreate(TournamentBase):
+    pass
+
+class TournamentRead(TournamentBase):
+    id: uuid.UUID
+
+
